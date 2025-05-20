be2f4af8f5d56fae42b928197d28b55d230769fb
hotrice37 <shubinpokhrel4@gmail.com>
2025-05-20 10:37:18 +0545

    Spark with OpenLineage works

--- dags/higgs_twitter_etl_dag.py
+++ dags/higgs_twitter_etl_dag.py
@@ -226,6 +226,13 @@ convert_to_parquet = SparkSubmitOperator(
     name="higgs_parquet_conversion",
     trigger_rule="none_failed",  # Run this task when all direct upstream tasks have succeeded or been skipped
     verbose=True,
+    conf={
+        "spark.jars.packages": "io.openlineage:openlineage-spark_2.12:1.32.0",
+        "spark.extraListeners": "io.openlineage.spark.agent.OpenLineageSparkListener",
+        "spark.openlineage.transport.url": "http://host.docker.internal:5000/api/v1/lineage",
+        "spark.openlineage.transport.type": "http",
+        "spark.openlineage.namespace": "spark_integration"
+    },
     dag=dag,
 )
 
@@ -242,6 +249,13 @@ run_analysis = SparkSubmitOperator(
     name="higgs_analysis",
     on_execute_callback=None,  # No special pre-execution actions
     verbose=True,
+    conf={
+        "spark.jars.packages": "io.openlineage:openlineage-spark_2.12:1.32.0",
+        "spark.extraListeners": "io.openlineage.spark.agent.OpenLineageSparkListener",
+        "spark.openlineage.transport.url": "http://host.docker.internal:5000/api/v1/lineage",
+        "spark.openlineage.transport.type": "http",
+        "spark.openlineage.namespace": "spark_integration"
+    },
     dag=dag,
 )
 
